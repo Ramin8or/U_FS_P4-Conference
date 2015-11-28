9523bf0c2b6a09c0cab9b72559a1ed3650943a13
Ramin Halviatti <ramin@outlook.com>
2015-11-27 17:26:29 -0800

    Initial changes to get Udacity ConferenceCentral working on Google App Engine

---
+++ settings.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""settings.py
+
+Udacity conference server-side Python App Engine app user settings
+
+$Id$
+
+created/forked from conference.py by wesc on 2014 may 24
+
+"""
+
+# Replace the following lines with client IDs obtained from the APIs
+# Console or Cloud Console.
+WEB_CLIENT_ID = '265329550305-0iqmts0903vh3qks0g6t54h7doatv4on.apps.googleusercontent.com'
+ANDROID_CLIENT_ID = 'replace with Android client ID' # TODO
+IOS_CLIENT_ID = 'replace with iOS client ID'		 # TODO
+ANDROID_AUDIENCE = WEB_CLIENT_ID
